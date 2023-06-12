e6639c15b4b56ed0e5179ae62fe623c2e9792673
Mohamed-Talaat <eng.muhamedtalaat@gmail.com>
2023-06-12 10:23:12 +0300

    add second way to format the original ip

--- routes/fetchPublicIP.js
+++ routes/fetchPublicIP.js
@@ -12,6 +12,21 @@ router.get('/fetch-public-ip', (req, res) => {
         res.send({ip});
 });
 
+router.get('/fetch-public-ip-v2', (req, res) => {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    // If there are multiple IP addresses in x-forwarded-for, the first one is the original IP address
+    if (ip.indexOf(',') > -1) {
+        ip = ip.split(',')[0].trim();
+    }
+
+    if (ip.substr(0, 7) == "::ffff:") {
+        ip = ip.substr(7)
+    }
+    res.send({ip});
+});
+
+
 router.get('/checkUserUsingDNS',(req,res)=>{
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     if (ip.substr(0, 7) == "::ffff:") {
