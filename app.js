dd0db9ac18f91e5fd3628ac789bfd69cd74568e5
Mohamed-Talaat <eng.muhamedtalaat@gmail.com>
2023-09-19 16:32:17 +0300

    remove dotenv

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var cors = require('cors');
-const express = require('express');
+var cors = require("cors");
+const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.set("trust proxy", "loopback");
+app.use(express.json());
 app.use(cors());
+
 //Here we are configuring express to use body-parser as middle-ware.
 
 //initialize
-const port = 3000
-app.use('/api', require('./routes/api'));
-app.use('/watania', require('./routes/fetchPublicIP'));
+const port = 4000;
+
+app.use("/api", require("./routes/api"));
+app.use("/attendance-ip", require("./routes/fetchPublicIP"));
+app.use("/", (req, res) => {
+  res.send(`server is working on port ${port}`);
+});
 
 //listen for requests
-app.listen(process.env.port || port, () => console.log(`server start on port ${port}`))
+app.listen(process.env.port || port, () =>
+  console.log(`server start on port ${port}`)
+);
